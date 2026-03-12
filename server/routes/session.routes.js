d21ac9e98537016f0ace3517b5d173e47195535d
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2026-03-12 11:28:12 -0400

    Feat: signup and session endpoints work, test with postman

--- server/routes/session.routes.js
+++ server/routes/session.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const auth = require("../middleware/auth.js");
-const { checkSession, createSession, logout } = require('../controllers/session.controller.js');
+const { createSession } = require('../controllers/session.controller.js');
 
 const router = express.Router();
 
-router.get('/', auth, checkSession);
+// router.get('/', auth, checkSession);
 router.post('/', createSession);
-router.post('/logout', logout);
+// router.post('/logout', logout);
 
 module.exports = router;
