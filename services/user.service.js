0215de0a78f8aa8b1359e047e22d4ccb061cff36
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2026-03-12 11:28:12 -0400

    Feat: signup and session endpoints work, test with postman

--- services/user.service.js
+++ services/user.service.js
@@ -1,12 +1,12 @@
 const connection = require('../db.js');
 const bcrypt = require('bcryptjs');
 
-async function create(username, firstName, lastName, password, email) {
+async function create(username, password, email) {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   const [result] = await connection.promise().query(
     `INSERT INTO users (username, password, email)
-    VALUES (?, ?, ?, ?, ?);`,
+    VALUES (?, ?, ?);`,
     [username, hashedPassword, email]
   );
 
