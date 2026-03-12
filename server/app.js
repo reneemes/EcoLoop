5b0716db7783704e02e82012d0e9607b56fdb337
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2026-03-12 11:21:01 -0400

    Feat: basic user and session creation, database connection, package installation

---
+++ server/app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+// Import route files
+const sessionRoutes = require('./routes/session.routes.js');
+const userRoutes = require('./routes/user.routes.js');
+
+// create an express application
+const app = express();
+
+// middleware → allows cross-origin requests
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+// allows us to parse JSON data from requests
+app.use(express.json());
+// allows you to read the cookies. Example: req.cookies.token
+app.use(cookieParser());
+
+app.use('/api/v1/session', sessionRoutes);
+app.use('/api/v1/user', userRoutes);
+
+module.exports = app;
---
+++ server/services/session.service.js
@@ -0,0 +1,44 @@
+const connection = require('../db.js');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+async function create(username, password) {
+  const [result] = await connection.promise().query(
+    `SELECT id, username, email, password
+    FROM users
+    WHERE LOWER(username) = LOWER(?);`,
+    [username]
+  );
+
+  const user = result[0];
+
+  if (!user) {
+    throw new Error('Invalid credentials');
+  }
+  
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!user || !passwordMatch) {
+    throw new Error('Invalid credentials');
+  }
+
+  const token = jwt.sign(
+    {id: user.id},
+    process.env.JWT_SECRET,
+    { expiresIn: '1h' }
+  );
+
+  return {
+    token,
+    user: {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+    }
+  }
+}
+
+module.exports = {
+  // show,
+  create
+};
