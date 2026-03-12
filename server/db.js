f8237fcfabe9c9e7541716408591fbb3211c5d41
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2026-03-12 15:07:36 -0400

    Test: session controller unit tests, happy and sad paths

--- server/db.js
+++ server/db.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-});
+function createConnection() {
+  return mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+  });
+}
 
-module.exports = connection;
+module.exports = createConnection;
