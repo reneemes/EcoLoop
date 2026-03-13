050052b45b4fbac1801b153eb7e1946417e5d38b
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-03-13 10:07:57 -0400

    Test: integration test for session controller and service, 100% coverage

--- server/tests/integration/session.controller.int.test.js
+++ server/tests/integration/session.controller.int.test.js
@@ -4,17 +4,67 @@ const userLogin = require('../mock-data/new-user-session.json');
 
 const endpointUrl = '/api/v1/session';
 
+let mockUser = { userId: 3 };
+
+jest.mock('../../middleware/auth', () => {
+  return (req, res, next) => {
+    req.user = mockUser;
+    next();
+  };
+});
+
 describe(endpointUrl, () => {
   it('GET ' + endpointUrl, async () => {
-    user = { userId: '3' };
+    mockUser = { userId: 3 };
+
     const response = await request(app)
-      .get(endpointUrl)
-      .send(user);
+      .get(endpointUrl);
 
     expect(response.statusCode).toBe(200);
-    // expect(response.body.message).toBe('Sign in successful');
-    // expect(response.body.user.username).toBe(userLogin.username);
-    // expect(response.body.user.email).toBe(userLogin.email);
+    expect(response.body.user.id).toBe(3);
+    expect(response.body.user.username).toBe('test-user');
+    expect(response.body.user.email).toBe('test@example.com');
+  });
+
+  it('should return 401 if user is not authenticated', async () => {
+    mockUser = undefined;
+
+    const response = await request(app)
+      .get(endpointUrl);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toStrictEqual({
+      message: 'Not authenticated'
+    });
+  });
+
+  it('should return 401 if userId is missing', async () => {
+    mockUser = {};
+    const response = await request(app)
+      .get(endpointUrl);
+
+    expect(response.statusCode).toBe(401);
+  });
+
+  it('should return 401 if the user does not exist', async () => {
+    const response = await request(app)
+      .get(endpointUrl);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toStrictEqual({
+      message: 'Not authenticated'
+    });
+  });
+
+  it('should return 500 if the user does not exist in the database', async () => {
+    mockUser = { userId: 99 };
+    const response = await request(app)
+      .get(endpointUrl);
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toStrictEqual({
+      message: 'Failed to fetch user'
+    });
   });
 })
 
