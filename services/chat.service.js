ec5ea2f11c51df3b2e3036a7945915e265558ad7
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-03-15 10:30:19 -0400

    Feat: add auth to chat routes to save user search history

--- services/chat.service.js
+++ services/chat.service.js
@@ -1,15 +1,14 @@
 const { GoogleGenAI } = require("@google/genai");
+const createConnection = require('../db.js');
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function create(prompt) {
-  try{
-    console.log('here')
+  try {
     const response = await ai.models.generateContent({
       model: "gemini-3-flash-preview",
       contents: prompt,
     });
-    console.log(response.text);
     return response.text;
   } catch (error) {
     console.error(error);
@@ -17,6 +16,19 @@ async function create(prompt) {
   }
 }
 
+async function save(userId, keyword, chat) {
+  const db = await createConnection();
+  
+  const [result] = await db.promise().query(
+    `INSERT INTO search_history (user_id, keyword, result)
+    VALUES (?, ?, ?);`,
+    [userId, keyword, chat]
+  );
+  console.log('result: ', result);
+  return result[0];
+}
+
 module.exports = {
   create,
+  save,
 }
