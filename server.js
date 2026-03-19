37f8429d8f732cb7f1349be21740940822b28e55
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2026-03-19 15:18:38 -0400

    Feat: AWS config

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = require("./app");
 
 // start server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
