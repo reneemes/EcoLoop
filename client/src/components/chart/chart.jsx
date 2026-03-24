90405bbcc45363d70caae595273d97ac03514082
Renee Messersmith <reneemessersmith@Administrators-MacBook-Pro.local>
2026-03-24 14:52:13 -0400

    Feat: add google charts

--- client/src/components/chart/chart.jsx
+++ client/src/components/chart/chart.jsx
@@ -3,14 +3,12 @@ import { Chart } from 'react-google-charts';
 function MyChart({ data }) {
   return (
     <Chart
-      // Try different chart types by changing this property with one of: LineChart, BarChart, AreaChart...
       chartType='ColumnChart'
       data={data}
       options={{
         title: 'Recycling Overview',
         legend: { position: 'none' },
       }}
-      // legendToggle
     />
   );
 }
