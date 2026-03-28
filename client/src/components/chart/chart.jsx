1a79e48b4773d172cd4b20030c5923c0cc3786fa
Renee Messersmith <reneemessersmith@AdminisatorsMBP.lan>
2026-03-27 21:49:32 -0400

    Feat: fixing dashboard for desktop

--- client/src/components/chart/chart.jsx
+++ client/src/components/chart/chart.jsx
@@ -3,12 +3,27 @@ import { Chart } from 'react-google-charts';
 function MyChart({ data }) {
   return (
     <Chart
-      chartType='ColumnChart'
+      chartType='PieChart'
       data={data}
       options={{
         title: 'Recycling Overview',
-        legend: { position: 'none' },
+        legend: {
+          position: "bottom",
+          alignment: "center",
+          textStyle: {
+            color: "#233238",
+            fontSize: 14,
+          },
+        },
+        colors: ["#a8e6cf", "#dcedc1", "#ffd3b6","#ffaaa5", "#c7ceea"],
+        chartArea: {
+        left: 30,    // padding inside chart container
+        top: 50,
+        width: '90%',
+        // height: '70%',
+      },
       }}
+      //  width={'75%'}
     />
   );
 }
